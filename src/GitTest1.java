695cc9c8585e4c74d1b9966943b9fd137f5ccbbe
sivasurya14 <ELCOT@Lenovo>
2022-01-20 10:18:40 +0530

    first commit

---
+++ src/GitTest1.java
@@ -0,0 +1,13 @@
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+
+		int per = 100;
+
+		if (per == 100)
+			System.out.println("good quality");
+		else
+			System.out.println("need to improve");
+	}
+}
